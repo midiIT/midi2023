96dea0e42ba8aa81bdfd02a767142fbfc7bdb6ed
gustaz <gustas2001@mail.ru>
2023-01-30 02:38:03 +0200

    working example of carousel, generally huge tweaks to website flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
-import Navbar from "../components/Navbar";
+import Carousel from "../components/Carousel";
 
 export default function Index() {
   return (
@@ -11,48 +11,36 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar></Navbar>
-      <div id="start" className="h-screen bg-slate-900">
-        <div className="flex h-full flex-col items-center justify-center">
-          <p className="m-auto text-6xl text-midiblue md:text-8xl">MIDI 2023</p>
-        </div>
-      </div>
-      <div id="about" className="h-screen bg-gray-800">
-        <div className="flex h-full flex-col items-center justify-center">
-          <p className="m-auto text-6xl text-midiblue md:text-8xl">ABOUT</p>
-        </div>
-      </div>
-      <div id="events" className="h-screen bg-slate-900">
-        <div className="flex h-full flex-col items-center justify-center">
-          <p className="m-auto text-6xl text-midiblue md:text-8xl">EVENTS</p>
-        </div>
-      </div>
-      <div id="team" className="h-screen bg-gray-800 p-4">
-        <div className="flex h-full flex-col items-center justify-center">
-          <p className="mb-16 text-6xl text-midiblue md:text-8xl">TEAM</p>
-          <div className="grid grid-flow-col grid-rows-2 gap-16 text-white md:gap-8 lg:grid-rows-1">
-            <Avatar
-              name="Gustas"
-              title="IT koordinatorius"
-              imgsrc="/media/Gustas.webp"
-            ></Avatar>
-            <Avatar
-              name="Gustas"
-              title="IT kombinatorius"
-              imgsrc="/media/Gustas.webp"
-            ></Avatar>
-            <Avatar
-              name="Gustas"
-              title="IT rekombinatorius"
-              imgsrc="/media/Gustas.webp"
-            ></Avatar>
-            <Avatar
-              name="Gustas"
-              title="IT rekombinatorius"
-              imgsrc="/media/Gustas.webp"
-            ></Avatar>
-          </div>
-        </div>
+      <div className="flex flex-col sm:flex-row">
+        <Carousel></Carousel>
+        <section
+          id="events"
+          className="h-screen w-full bg-slate-900 text-white transition-opacity"
+        >
+          <Avatar
+            name="Gustas"
+            title="hello"
+            imgsrc="/media/Gustas.webp"
+          ></Avatar>
+        </section>
+        <section
+          id="team"
+          className="invisible hidden h-screen w-full bg-slate-900 text-white opacity-0 transition-opacity"
+        >
+          <Avatar name="it me" title="yes" imgsrc="/media/Gustas.webp"></Avatar>
+        </section>
+        <section
+          id="sponsors"
+          className="invisible hidden h-screen w-full bg-slate-900 text-white opacity-0 transition-opacity"
+        >
+          <Avatar name="=)" title="lol" imgsrc="/media/Gustas.webp"></Avatar>
+        </section>
+        <section
+          id="articles"
+          className="invisible hidden h-screen w-full bg-slate-900 text-white opacity-0 transition-opacity"
+        >
+          MNP
+        </section>
       </div>
     </>
   );
