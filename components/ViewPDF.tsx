04bff3edf5d7e4930bc456c6f99a01ba9b9d6a75
gustaz <gustas2001@mail.ru>
2023-02-22 19:23:51 +0200

    ViewPDF import set to dynamic, minor error fixed in eventsSection

--- components/ViewPDF.tsx
+++ components/ViewPDF.tsx
@@ -1,43 +1,38 @@
-import React, { useState } from "react";
-// import pdfjs from "pdfjs-dist";
-// const {Document, Page, pdfjs} = require('@types/react-pdf')
-// import { pdfjs } from 'react-pdf';
-// pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
-import { Document, Page, pdfjs} from 'react-pdf/dist/esm/entry.webpack5';
-// import dynamic from 'next/dynamic'
-
-// const reactPDF = dynamic(import("@react-pdf"), { ssr: false })
-// import { pdfjs } from 'react-pdf';
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
-// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const options = {
-    cMapUrl: "cmaps/",
-    cMapPacked: true,
+  cMapUrl: "cmaps/",
+  cMapPacked: true,
 };
 
 export default function ViewPDF() {
-    const [file, setFile] = useState("../media/test.pdf");
-    const [numPages, setNumPages] = useState(null);
-    // console.log('test');
-    function onDocumentLoadSuccess({ numPages: nextNumPages }: any) {
-        setNumPages(nextNumPages);
-    }
+  const [file, setFile] = useState("../media/test.pdf");
+  const [numPages, setNumPages] = useState(null);
+  // console.log('test');
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: any) {
+    setNumPages(nextNumPages);
+  }
 
-    return(
-        <div>
-            <header>
-                <h1>React-pdf</h1>
-            </header>
-            <div>
-                <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-                    {Array.from(new Array(numPages), (el, index) => (
-                        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-                    ))}
-                </Document>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <h1>React-pdf</h1>
+      </header>
+      <div>
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      </div>
+    </div>
+  );
 }
 
 // import React, { useState } from "react";
--- components/sections/articleSection.tsx
+++ components/sections/articleSection.tsx
@@ -1,6 +1,7 @@
-import { View } from "@react-pdf/renderer";
-import React from "react";
-import ViewPDF from "../ViewPDF";
+import dynamic from "next/dynamic";
+const ViewPDF = dynamic(() => import("../ViewPDF"), {
+  ssr: false,
+});
 
 export default function ArticleSection(props: {
   name: string;
@@ -8,11 +9,16 @@ export default function ArticleSection(props: {
 }) {
   return (
     <div className="flex flex-col text-xl sm:text-2xl md:text-2xl lg:text-3xl">
-      <button className="flex space-x-4 text-left items-center" onClick={() => <ViewPDF/>}>
-        <img className="float-left h-24 w-24 sm:h-32 sm:w-32 md:h-36 md:w-36" src="/media/placeholder.png" alt="placeholder"/>
-        <span>
-          {props.name}
-        </span>
+      <button
+        className="flex items-center space-x-4 text-left"
+        onClick={() => <ViewPDF />}
+      >
+        <img
+          className="float-left h-24 w-24 sm:h-32 sm:w-32 md:h-36 md:w-36"
+          src="/media/placeholder.png"
+          alt="placeholder"
+        />
+        <span>{props.name}</span>
       </button>
     </div>
   );
