b011c3b55b096db7fca9b118936093d5f63924d1
gustaz <gustas2001@mail.ru>
2023-03-23 17:29:52 +0200

    localization fixes

--- components/sections/eventsSection.tsx
+++ components/sections/eventsSection.tsx
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 interface Event {
   name: string;
   description: string;
@@ -8,51 +10,60 @@ interface Props {
   events: Event[];
 }
 
-var events=[
+var events = [
   {
     name: "Sportinio Pokerio Turnyras",
-    description: "Sportinio pokerio turnyras yra puiki vieta parungtyniauti su kitais pokerio mėgėjais ar smagiai praleisti vakarą populiariausiame Vilniaus sportinio pokerio klube.",
-    time: new Date("2023-04-24T18:30:00Z"),
+    description:
+      "Sportinio pokerio turnyras yra puiki vieta parungtyniauti su kitais pokerio mėgėjais ar smagiai praleisti vakarą populiariausiame Vilniaus sportinio pokerio klube.",
+    time: format(new Date("2023-04-24T18:30:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Krepšinio Turnyras",
-    description: "MIDI 2023 3x3 krepšinio turnyras vėl čia! Tai puiki proga išbandyti savo jėgas aikštelėje ar tiesiog linksmai ir aktyviai praleisti laiką su draugais.",
-    time: new Date("2023-04-25T20:00:00Z"),
+    description:
+      "MIDI 2023 3x3 krepšinio turnyras vėl čia! Tai puiki proga išbandyti savo jėgas aikštelėje ar tiesiog linksmai ir aktyviai praleisti laiką su draugais.",
+    time: format(new Date("2023-04-25T20:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Tinklinio Turnyras",
-    description: "Su kamuoliu prie kojų jautiesi lyg Messi ar Ronaldo, gal atrandi save gindamas vartus kaip G. Bufonas, o gal prisilietes prie kamuolio grįžti į vaikyste, kuomet su draugais praleisdavai dienų dienas pievoje? Bet kuriuo atveju MIDI 2023 futbolo turnyras kaip tik tau, surink savo komanda ir stok į kovą su kitais futbolo mylėtojais.  ",
-    time: new Date("2023-04-25T20:00:00Z"),
+    description:
+      "Su kamuoliu prie kojų jautiesi lyg Messi ar Ronaldo, gal atrandi save gindamas vartus kaip G. Bufonas, o gal prisilietes prie kamuolio grįžti į vaikyste, kuomet su draugais praleisdavai dienų dienas pievoje? Bet kuriuo atveju MIDI 2023 futbolo turnyras kaip tik tau, surink savo komanda ir stok į kovą su kitais futbolo mylėtojais.  ",
+    time: format(new Date("2023-04-25T20:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Futbolo Turnyras",
-    description: "Su kamuoliu prie kojų jautiesi lyg Messi ar Ronaldo, gal atrandi save gindamas vartus kaip G. Bufonas, o gal prisilietes prie kamuolio grįžti į vaikyste, kuomet su draugais praleisdavai dienų dienas pievoje? Bet kuriuo atveju MIDI 2023 futbolo turnyras kaip tik tau, surink savo komanda ir stok į kovą su kitais futbolo mylėtojais.  ",
-    time: new Date("2023-04-25T20:00:00Z"),
+    description:
+      "Su kamuoliu prie kojų jautiesi lyg Messi ar Ronaldo, gal atrandi save gindamas vartus kaip G. Bufonas, o gal prisilietes prie kamuolio grįžti į vaikyste, kuomet su draugais praleisdavai dienų dienas pievoje? Bet kuriuo atveju MIDI 2023 futbolo turnyras kaip tik tau, surink savo komanda ir stok į kovą su kitais futbolo mylėtojais.  ",
+    time: format(new Date("2023-04-25T20:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Įmonių Mugė",
-    description: "Įmonių mugė puiki proga įmonėms prisistatyti tikslinei auditorijai, o studentams susipažinti su būsimomis darbovietėmis bei ieškoti praktikos galimybių. Prieš įmonių mugės pradžią, pastato savo stendus, roll up ir t.t Naugarduko fakulteto ilgajame koridoriuje ir renginio metu stengiasi pritraukti studentus įvairiais konkursais ir žaidimais. Pertraukų metu įmonių atstovai ir studentai bus kviečiami pasikalbėti prie arbatos/kavos puodelio, kur galės laisviau pakalbėti ar net susipažinti, taip užmezgant geresnį ryšį",
-    time: new Date("2023-04-24T19:00:00Z"),
+    description:
+      "Įmonių mugė puiki proga įmonėms prisistatyti tikslinei auditorijai, o studentams susipažinti su būsimomis darbovietėmis bei ieškoti praktikos galimybių. Prieš įmonių mugės pradžią, pastato savo stendus, roll up ir t.t Naugarduko fakulteto ilgajame koridoriuje ir renginio metu stengiasi pritraukti studentus įvairiais konkursais ir žaidimais. Pertraukų metu įmonių atstovai ir studentai bus kviečiami pasikalbėti prie arbatos/kavos puodelio, kur galės laisviau pakalbėti ar net susipažinti, taip užmezgant geresnį ryšį",
+    time: format(new Date("2023-04-24T19:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Protmūšis",
-    description: "Įmonių mugė puiki proga įmonėms prisistatyti tikslinei auditorijai, o studentams susipažinti su būsimomis darbovietėmis bei ieškoti praktikos galimybių. Prieš įmonių mugės pradžią, pastato savo stendus, roll up ir t.t Naugarduko fakulteto ilgajame koridoriuje ir renginio metu stengiasi pritraukti studentus įvairiais konkursais ir žaidimais. Pertraukų metu įmonių atstovai ir studentai bus kviečiami pasikalbėti prie arbatos/kavos puodelio, kur galės laisviau pakalbėti ar net susipažinti, taip užmezgant geresnį ryšį",
-    time: new Date("2023-04-27T19:00:00Z"),
+    description:
+      "Įmonių mugė puiki proga įmonėms prisistatyti tikslinei auditorijai, o studentams susipažinti su būsimomis darbovietėmis bei ieškoti praktikos galimybių. Prieš įmonių mugės pradžią, pastato savo stendus, roll up ir t.t Naugarduko fakulteto ilgajame koridoriuje ir renginio metu stengiasi pritraukti studentus įvairiais konkursais ir žaidimais. Pertraukų metu įmonių atstovai ir studentai bus kviečiami pasikalbėti prie arbatos/kavos puodelio, kur galės laisviau pakalbėti ar net susipažinti, taip užmezgant geresnį ryšį",
+    time: format(new Date("2023-04-27T19:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Šachmatai",
-    description: "MIDI šachmatų turnyras vėl čia! Nesvarbu, ar esi tik pradedantysis šachmatų žaidėjas, ar tavo įgūdžiai prilygsta Hikaru Nakamurai, šis turnyras yra puiki proga pamankštinti smegenis ir varžytis su šviesiausiais Vilniaus protais!",
-    time: new Date("2023-04-23T19:00:00Z"),
+    description:
+      "MIDI šachmatų turnyras vėl čia! Nesvarbu, ar esi tik pradedantysis šachmatų žaidėjas, ar tavo įgūdžiai prilygsta Hikaru Nakamurai, šis turnyras yra puiki proga pamankštinti smegenis ir varžytis su šviesiausiais Vilniaus protais!",
+    time: format(new Date("2023-04-23T19:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Uždarymo Vakaras",
-    description: "Uždarymo vakaras - tai visą MIDI veiklų maratoną vainikuojantis renginys. Jo metu dalyviai galės ne tik dalintis patirtais įspūdžiais, atradimais ar įgytomis žiniomis, bet ir įsitraukti į veiklas, kurios padės MIDI prisiminti dar ilgai. O, kad pokalbiai nepabostų, nuo jų atsitraukti ir pajudėti paskatins lietuviškos grupės, po kurių tikrai nereikėtų pabėgti, nes vakarą triukšmingai užbaigs - DJ, kuris patenkins visus dalyvių norus. ",
-    time: new Date("2023-04-28T19:00:00Z"),
+    description:
+      "Uždarymo vakaras - tai visą MIDI veiklų maratoną vainikuojantis renginys. Jo metu dalyviai galės ne tik dalintis patirtais įspūdžiais, atradimais ar įgytomis žiniomis, bet ir įsitraukti į veiklas, kurios padės MIDI prisiminti dar ilgai. O, kad pokalbiai nepabostų, nuo jų atsitraukti ir pajudėti paskatins lietuviškos grupės, po kurių tikrai nereikėtų pabėgti, nes vakarą triukšmingai užbaigs - DJ, kuris patenkins visus dalyvių norus. ",
+    time: format(new Date("2023-04-28T19:00:00"), "yyyy-MM-dd HH:mm"),
   },
   {
     name: "Orientacinės",
-    description: "Komandinės MIDI 2023 orientacinės varžybos “Kas nutiko vakar naktį?”. Klaidžiojant Vilniaus gatvėmis dalyviai bandys sudėlioti paslaptingą istoriją apie vakarykštės nakties įvykius. Komandoms prireiks papildomų priemonių, kaip internetas ir išmanieji telefonai bei “Discord” programėlės. Žibintuvėlis, susiorientuoti tamsiuose vietose - būtinas, o popieriaus lapelis ir rašymo priemonė gali retkarčiais praversti. Žaidimo pabaigoje daugiausiai taškų surinkusios trys komandos bus apdovanotos prizais.",
-    time: new Date("2023-04-21T19:00:00Z"),
+    description:
+      "Komandinės MIDI 2023 orientacinės varžybos “Kas nutiko vakar naktį?”. Klaidžiojant Vilniaus gatvėmis dalyviai bandys sudėlioti paslaptingą istoriją apie vakarykštės nakties įvykius. Komandoms prireiks papildomų priemonių, kaip internetas ir išmanieji telefonai bei “Discord” programėlės. Žibintuvėlis, susiorientuoti tamsiuose vietose - būtinas, o popieriaus lapelis ir rašymo priemonė gali retkarčiais praversti. Žaidimo pabaigoje daugiausiai taškų surinkusios trys komandos bus apdovanotos prizais.",
+    time: format(new Date("2023-04-21T19:00:00"), "yyyy-MM-dd HH:mm"),
   },
 ];
 
@@ -60,13 +71,20 @@ const EventList: React.FC<Props> = () => {
   return (
     <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
       {events.map((event) => (
-        <div key={event.name} className="border border-gray-200 rounded-lg shadow-sm">
-          <div className="bg-gray-100 px-4 py-3 rounded-t-lg">
-            <h3 className="text-lg font-semibold text-gray-800">{event.name}</h3>
+        <div
+          key={event.name}
+          className="rounded-lg border border-gray-200 shadow-sm"
+        >
+          <div className="rounded-t-lg bg-gray-100 px-4 py-3">
+            <h3 className="text-lg font-semibold text-gray-800">
+              {event.name}
+            </h3>
           </div>
           <div className="px-4 py-3">
             <p className="text-gray-700">{event.description}</p>
-            <p className="mt-2 text-sm text-gray-500">{event.time.toLocaleString()}</p>
+            <p className="mt-2 text-sm text-gray-500">
+              {event.time.toLocaleString()}
+            </p>
           </div>
         </div>
       ))}
@@ -75,7 +93,6 @@ const EventList: React.FC<Props> = () => {
 };
 
 export default EventList;
-
 
 // import Event from "../Event";
 // export default function EventsSection() {
