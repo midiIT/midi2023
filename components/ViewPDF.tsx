5812408494614331ca9e61d580c93f012e018f6c
Olen <olen2003@gmail.com>
2023-03-23 20:08:09 +0200

    Removed some comments

--- components/ViewPDF.tsx
+++ components/ViewPDF.tsx
@@ -14,11 +14,6 @@ export default function ViewPDF(props: { pdfLocation: string }) {
   const [file, setFile] = useState(props.pdfLocation);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  // console.log('test');
-
-  //   function onDocumentLoadSuccess({ numPages: nextNumPages }: any) {
-  //     setNumPages(nextNumPages);
-  //   }
   const size = useWindowSize();
 
   function onDocumentLoadSuccess({ numPages }: any) {
@@ -37,41 +32,7 @@ export default function ViewPDF(props: { pdfLocation: string }) {
   function nextPage() {
     changePage(1);
   }
-  // PDF with next and previous buttons
-  // return (
-  //   <div className="flex flex-col items-center w-screen z-10 mt-[5%] overflow-scroll">
-  //     <Document
-  //       file={props.pdfLocation}
-  //       onLoadSuccess={onDocumentLoadSuccess}
-  //     >
-  //       <div onClick={e => {e.stopPropagation();}}>
-  //       <Page pageNumber={pageNumber} renderAnnotationLayer={false} renderTextLayer={false}/>
-  //       </div>
-  //     </Document>
-  //     <div onClick={e => {e.stopPropagation();}}>
-  //       <p className="text-white">
-  //         Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-  //       </p>
-  //       <button
-  //         className="text-white"
-  //         type="button"
-  //         disabled={pageNumber <= 1}
-  //         onClick={previousPage}
-  //       >
-  //         Previous
-  //       </button>
-  //       <button
-  //         className="text-white"
-  //         type="button"
-  //         disabled={pageNumber >= numPages}
-  //         onClick={nextPage}
-  //       >
-  //         Next
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
-  // Continuous PDF
+
   return (
     <div className="fixed top-0 bottom-0 z-10 flex w-screen flex-col items-center overflow-y-scroll pt-[5%] pb-[5%]">
       <Document
@@ -178,43 +139,3 @@ function useWindowSize() {
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
 }
-// #ResumeContainer {
-//     margin:auto;
-//     width: 65%;
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-// }
-
-// import React, { useState } from "react";
-// import Loader from "./Loader";
-// import { Document, Page, pdfjs } from "react-pdf";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-// const ViewPDF = () => {
-//     const [numPages, setNumPages] = useState(null);
-//     const [pageNumber, setPageNumber] = useState(1);
-//     const [isLoading, setIsLoading] = useState(true);
-
-//     function onDocumentLoadSuccess({ numPages }: any) {
-//         setNumPages(numPages);
-//         setIsLoading(false);
-//     }
-
-//     return(
-//         <div>
-//             <Loader isLoading={isLoading} />
-//             <section
-//                 id="pdf-section"
-//                 className="flex flex-col justify-center items-center">
-//                 <Document
-//                     file="/media/placeholder.pdf"
-//                     onLoadSuccess={onDocumentLoadSuccess}>
-//                     <Page pageNumber={pageNumber} />
-//                 </Document>
-//             </section>
-//         </div>
-//     );
-// };
-
-// export default ViewPDF;
