333ce9627fab400566c6de76ed4c905be26b3f55
gustaz <gustas2001@mail.ru>
2023-03-23 21:18:43 +0200

    WIP on sponsors

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image"
+import Image from "next/image";
 export default function Sponsor(props: {
-    href: string,
-    width: number,
-    height: number,
-    src: string,
-    alt: string
-}){
-    return(
-    <a target="_blank" href={props.href}><Image width={props.width} height={props.height} src={props.src} alt={props.alt}></Image></a>
-    )
+  href: string;
+  width: number;
+  height: number;
+  src: string;
+  alt: string;
+}) {
+  return (
+    <a target="_blank" href={props.href}>
+      <Image
+        width={props.width}
+        height={props.height}
+        src={props.src}
+        alt={props.alt}
+      ></Image>
+    </a>
+  );
 }
--- components/sections/sponsorsSection.tsx
+++ components/sections/sponsorsSection.tsx
@@ -1,24 +1,98 @@
-import Image from "next/image"
-import Sponsor from "../Sponsor"
-export default function SponsorsSection(){
-    return(<div>
-        <p className = "text-center text-7xl">Auksiniai rėmėjai</p>
-        <div className = "grid grid-cols-6 gap-8 justify-items-center align-middle">
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/favicon.ico" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/favicon.ico" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/favicon.ico" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/favicon.ico" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/favicon.ico" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/favicon.ico" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/download.png" alt="midi ikona"></Image></a>
-            <a target="_blank" href="https://www.google.lt"><Image width={1500} height={1500} src="/sponsors/ustukiu_malunas_logo-01.png" alt="midi ikona"></Image></a>
-            <Sponsor href="https://www.lrt.lt" width={1500} height={1500} src="/sponsors/download.png" alt="necentruotas"></Sponsor>
-            <div></div>
-        </div>
-        <p className = "text-center text-7xl">Sidabriniai rėmėjai</p>
-        <p className = "text-center text-7xl">Bronziniai rėmėjai</p>
-        <p className = "text-center text-7xl">Partneriai</p>
-        <h1>Sveiki</h1>
+import Image from "next/image";
+import Sponsor from "../Sponsor";
+
+var sponsors = [
+  {
+    href: "https://lt.asseco.com/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/asseco.png",
+    alt: "asseco logotipas",
+  },
+  {
+    href: "https://bankerait.lt/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/bankera.svg",
+    alt: "Bankera logotipas",
+  },
+  {
+    href: "https://www.bentley.com/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/bentley.png",
+    alt: "Bentley logo",
+  },
+  {
+    href: "https://www.devbridge.com/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/devbridge.png",
+    alt: "Devbridge logotipas",
+  },
+  {
+    href: "https://dreamcubator.club/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/dreamcubator.png",
+    alt: "DreamCubator logotipas",
+  },
+  {
+    href: "https://www.stat.gov.lt/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/ls.svg",
+    alt: "Lietuvos statistikos logotipas",
+  },
+  {
+    href: "https://www.luminor.lt/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/luminor.svg",
+    alt: "Luminor logotipas",
+  },
+  {
+    href: "http://www.nordcurrent.com/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/nordcurrent.png",
+    alt: "Nordcurrent logotipas",
+  },
+  {
+    href: "https://www.stat.gov.lt/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/ls.jpg",
+    alt: "Lietuvos statistikos logotipas",
+  },
+  {
+    href: "https://www.stat.gov.lt/",
+    width: 1500,
+    height: 1500,
+    src: "/sponsors/ls.jpg",
+    alt: "Lietuvos statistikos logotipas",
+  },
+];
+
+export default function SponsorsSection() {
+  return (
+    <div>
+      <p className="text-center text-7xl">Auksiniai rėmėjai</p>
+      <div className="m-8 grid grid-cols-4 place-items-center gap-16 align-middle">
+        {sponsors.map((sponsor) => (
+          <Sponsor
+            href={sponsor.href}
+            width={sponsor.width}
+            height={sponsor.height}
+            src={sponsor.src}
+            alt={sponsor.alt}
+          ></Sponsor>
+        ))}
+        <div></div>
+      </div>
+      <p className="text-center text-7xl">Sidabriniai rėmėjai</p>
+      <p className="text-center text-7xl">Bronziniai rėmėjai</p>
+      <p className="text-center text-7xl">Partneriai</p>
     </div>
-    )
+  );
 }
