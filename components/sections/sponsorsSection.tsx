ef9d4b2741bdc0c2c0c40e518ba0750bd22e668e
gustaz <gustas2001@mail.ru>
2023-04-01 14:55:49 +0300

    Sponsor section MVP

--- components/sections/sponsorsSection.tsx
+++ components/sections/sponsorsSection.tsx
@@ -1,76 +1,109 @@
-import Image from "next/image";
 import Sponsor from "../Sponsor";
+import Asseco from "public/sponsors/asseco.png";
+import Bankera from "public/sponsors/bankera.svg";
+import Wargaming from "public/sponsors/wargaming.png";
+import Telia from "public/sponsors/telia.svg";
+import WU from "public/sponsors/westernunion.png";
+import DreamCubator from "public/sponsors/dreamcubator.png";
+import Teltonika from "public/sponsors/teltonika.svg";
+import godDev from "public/sponsors/goddev.svg";
+import Bentley from "public/sponsors/bentley.png";
+import DevBridge from "public/sponsors/devbridge.png";
+import NordCurrent from "public/sponsors/nordcurrent.png";
+import MobilePay from "public/sponsors/mobilepay.png";
+import Luminor from "public/sponsors/luminor.svg";
+import TestDevLab from "public/sponsors/testdevlab.png";
 
-var sponsors = [
+var goldenSponsors = [
   {
     href: "https://lt.asseco.com/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/asseco.png",
+    src: Asseco,
     alt: "asseco logotipas",
+    className: "basis-1/2",
   },
   {
     href: "https://bankerait.lt/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/bankera.svg",
+    src: Bankera,
     alt: "Bankera logotipas",
+    className: "basis-1/2",
+  },
+];
+
+var silverSponsors = [
+  {
+    href: "https://eu.wargaming.net/",
+    src: Wargaming,
+    alt: "Wargaming logotipas",
+    className: "invert basis-1/4 scale-75",
   },
   {
+    href: "https://www.telia.lt/",
+    src: Telia,
+    alt: "Telia logotipas",
+    className: "basis-1/4 scale-75",
+  },
+  {
+    href: "https://www.westernunion.com/lt",
+    src: WU,
+    alt: "Western Union logotipas",
+    className: "basis-1/4 scale-75",
+  },
+  {
+    href: "https://dreamcubator.club/lt/",
+    src: DreamCubator,
+    alt: "DreamCubator logotipas",
+    className: "basis-1/3 scale-75",
+  },
+  {
+    href: "https://teltonika-iot-group.com/",
+    src: Teltonika,
+    alt: "Teltonika logotipas",
+    className: "basis-1/3 scale-75",
+  },
+  {
+    href: "https://www.god.de/en/about-us/god-lithuania/",
+    src: godDev,
+    alt: "GOD dev Lietuva logotipas",
+    className: "basis-1/3 scale-75",
+  },
+  {
+    href: "https://www.mobilepaygroup.com/about-us",
+    src: MobilePay,
+    alt: "MobilePay logotipas",
+    className: "basis-1/3 scale-75",
+  },
+];
+
+var bronzeSponsors = [
+  {
     href: "https://www.bentley.com/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/bentley.png",
+    src: Bentley,
     alt: "Bentley logo",
+    className: "basis-1/6 scale-75",
   },
   {
     href: "https://www.devbridge.com/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/devbridge.png",
+    src: DevBridge,
     alt: "Devbridge logotipas",
-  },
-  {
-    href: "https://dreamcubator.club/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/dreamcubator.png",
-    alt: "DreamCubator logotipas",
-  },
-  {
-    href: "https://www.stat.gov.lt/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/ls.svg",
-    alt: "Lietuvos statistikos logotipas",
-  },
-  {
-    href: "https://www.luminor.lt/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/luminor.svg",
-    alt: "Luminor logotipas",
+    className: "basis-1/6 scale-75",
   },
   {
     href: "http://www.nordcurrent.com/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/nordcurrent.png",
+    src: NordCurrent,
     alt: "Nordcurrent logotipas",
+    className: "basis-1/6 scale-50",
   },
   {
-    href: "https://www.stat.gov.lt/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/ls.jpg",
-    alt: "Lietuvos statistikos logotipas",
+    href: "https://www.luminor.lt/",
+    src: Luminor,
+    alt: "Luminor logotipas",
+    className: "basis-1/6 scale-75",
   },
   {
-    href: "https://www.stat.gov.lt/",
-    width: 1500,
-    height: 1500,
-    src: "/sponsors/ls.jpg",
-    alt: "Lietuvos statistikos logotipas",
+    href: "https://www.testdevlab.com/",
+    src: TestDevLab,
+    alt: "TestDevLab logotipas",
+    className: "basis-1/6 scale-75",
   },
 ];
 
@@ -78,20 +111,41 @@ export default function SponsorsSection() {
   return (
     <div>
       <p className="text-center text-7xl">Auksiniai rėmėjai</p>
-      <div className="m-8 grid grid-cols-4 place-items-center gap-16 align-middle">
-        {sponsors.map((sponsor) => (
+      <div className="mx-16 flex place-items-center justify-items-center gap-8 align-middle">
+        {goldenSponsors.map((sponsor, idx) => (
           <Sponsor
             href={sponsor.href}
-            width={sponsor.width}
-            height={sponsor.height}
             src={sponsor.src}
             alt={sponsor.alt}
+            key={idx}
+            className={sponsor.className}
           ></Sponsor>
         ))}
-        <div></div>
       </div>
       <p className="text-center text-7xl">Sidabriniai rėmėjai</p>
+      <div className="mx-16 mt-8 flex flex-wrap place-items-center justify-center justify-items-center gap-8 align-middle">
+        {silverSponsors.map((sponsor, idx) => (
+          <Sponsor
+            href={sponsor.href}
+            src={sponsor.src}
+            alt={sponsor.alt}
+            key={idx}
+            className={sponsor.className}
+          ></Sponsor>
+        ))}
+      </div>
       <p className="text-center text-7xl">Bronziniai rėmėjai</p>
+      <div className="m-8 flex flex-wrap place-items-center justify-items-center gap-8 align-middle">
+        {bronzeSponsors.map((sponsor, idx) => (
+          <Sponsor
+            href={sponsor.href}
+            src={sponsor.src}
+            alt={sponsor.alt}
+            key={idx}
+            className={sponsor.className}
+          ></Sponsor>
+        ))}
+      </div>
       <p className="text-center text-7xl">Partneriai</p>
     </div>
   );
